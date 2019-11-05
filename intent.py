bbb09d0588d7b2920040523e6cc6313dfbac9059
Prem Nath Dey <premnath981@gmail.com>
2019-11-05 12:46:08 +0530

    shopping/intent phython file updated

---
+++ intent.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 15:15:26 2017
+
+The user's intents definitions. It serves as abstraction to separate NLU processing
+logic from response processing routines. Allows to easy register in the system as many
+intents as it needed.
+
+@author: yaric
+"""
+
+from command import GreetCommand, AddItemCommand, ShowItemsCommand, ClearListCommand, ShowStatsCommand
+
+class Intent(object):
+    
+    def __init__(self, bot, intent_name, context):
+        """
+        Creates new intent for specified chatbot with given name
+        Arguments:
+            bot the chatbot
+            name the intent name
+            context the execution context holding configuration parameters
+        """
+        self.chatbot = bot
+        self.name = intent_name
+        self.context = context
+        self.commands = []
+        self.initCommands()
+        
+    def execute(self, nlu_data):
+        """
+        Executes given intent by applying appropriate command to the given
+        parsed NLU data response
+        """
+        for c in self.commands:
+            c.do(self.chatbot, None)
+    
+    def initCommands(self):
+        """
+        The method to init specific to particular intent.
+        """
+        pass
+    
+class AddItemsIntent(Intent):
+    
+    def initCommands(self):
+        self.commands.append(AddItemCommand())
+        self.commands.append(ShowItemsCommand())
+    
+    def execute(self, data):
+        confidence = data['intent']['confidence']
+        confidence_threshold = self.context['confidence_threshold']
+        if confidence < confidence_threshold:
+            print('I\'m sorry! Could you please paraphrase!')
+            return
+        
+        # add all intent entities
+        for entity in data['entities']:
+            self.commands[0].do(self.chatbot, entity['value'])
+        
+        # show items list
+        self.commands[1].do(self.chatbot, None)
+        
+class HelloIntent(Intent):
+    def initCommands(self):
+        self.commands.append(GreetCommand())
+    
+class ShowItemsIntent(Intent):
+    def initCommands(self):
+        self.commands.append(ShowItemsCommand())
+        
+class ClearListIntent(Intent):
+    def initCommands(self):
+        self.commands.append(ClearListCommand())
+        self.commands.append(ShowItemsCommand())
+        
+class ShowStatsIntent(Intent):
+     def initCommands(self):
+        self.commands.append(ShowStatsCommand())
+
+        
+    
---
+++ shopping_bot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 22:52:04 2017
+
+The shopping bot main class which provides routines to maintain NLU model,
+parse user's intents and execute corresponding commands in response.
+
+@author: yaric
+"""
+
+from rasa_nlu.converters import load_data
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+
+from intent import HelloIntent, AddItemsIntent, ClearListIntent, ShowItemsIntent, ShowStatsIntent
+
+class ShoppingBot(object):
+    def __init__(self, training_data_file = "./data/shopping-list/rasa/shopping-list-small.json",
+                 config_file = "./config/shopping-list/config_spacy.json"):
+        training_data = load_data(training_data_file)
+        
+        trainer = Trainer(RasaNLUConfig(config_file))
+        self.interpreter = trainer.train(training_data)
+        self.shopping_list = {}
+        
+        # Create supported intents
+        context = {'confidence_threshold':0.8}
+        self.intents = {
+                "greet"     : HelloIntent(self, "greet", context),
+                "add_item"  : AddItemsIntent(self, "add_item", context),
+                "clear_list": ClearListIntent(self, "clear_list", context),
+                "show_items": ShowItemsIntent(self, "show_items", context),
+                "_num_items": ShowStatsIntent(self, "_num_items", context)
+            }
+        
+    
+    def handle(self, message):
+        """
+        Handles incoming message using trained NLU model and prints response to
+        the system out
+        Arguments:
+            message the message from user to be handled with known intents 
+            (greet, add_item, clear_list, show_items, _num_items)
+        """
+        if message == '_num_items':
+            self.intents['_num_items'].execute(None)
+        else:
+            nlu_data = self.interpreter.parse(message)
+            intent = nlu_data['intent']['name']
+            if self.intents[intent] is not None:
+                self.intents[intent].execute(nlu_data)
+        
+
+        
